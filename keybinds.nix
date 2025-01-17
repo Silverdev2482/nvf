42397c0ec2015e797d7843d8570e101c6f53093c
silverdev2482 <fidget1206@gmail.com>
2025-01-17 15:15:30 -0600

    Formatting keybinds.nix

--- keybinds.nix
+++ keybinds.nix
@@ -19,64 +19,62 @@
 
     # Unbind Arrows and unused keys
 
-    { key = "w"; mode = [ "n" "o" "v" "x" ]; action = "<Nop>"; }
-
-    { key = "<Up>"; mode = [ "n" "o" "v" "x" ]; action = "<Nop>"; }
-    { key = "<Down>"; mode = [ "n" "o" "v" "x" ]; action = "<Nop>"; }
-    { key = "<Left>"; mode = [ "n" "o" "v" "x" ]; action = "<Nop>"; }
+    { key = "<Up>";    mode = [ "n" "o" "v" "x" ]; action = "<Nop>"; }
+    { key = "<Down>";  mode = [ "n" "o" "v" "x" ]; action = "<Nop>"; }
+    { key = "<Left>";  mode = [ "n" "o" "v" "x" ]; action = "<Nop>"; }
     { key = "<Right>"; mode = [ "n" "o" "v" "x" ]; action = "<Nop>"; }
 
 
 
     # Motions
 
-    { key = "n"; mode = [ "n" "o" "v" "x" ]; action = "h";   desc = "Left"; }
-    { key = "e"; mode = [ "n" "o" "v" "x" ]; action = "j";   desc = "Down"; }
-    { key = "i"; mode = [ "n" "o" "v" "x" ]; action = "k";   desc = "Up"; }
-    { key = "o"; mode = [ "n" "o" "v" "x" ]; action = "l";   desc = "Right"; }
+    { key = "n";       mode = [ "n" "o" "v" "x" ]; action = "h";   desc = "Left"; }
+    { key = "e";       mode = [ "n" "o" "v" "x" ]; action = "j";   desc = "Down"; }
+    { key = "i";       mode = [ "n" "o" "v" "x" ]; action = "k";   desc = "Up"; }
+    { key = "o";       mode = [ "n" "o" "v" "x" ]; action = "l";   desc = "Right"; }
 
-    { key = "l"; mode = [ "n" "o" "v" "x" ]; action = "b";   desc = "Previous Word"; }
-    { key = "u"; mode = [ "n" "o" "v" "x" ]; action = "w";   desc = "Next First Letter"; }
-    { key = "y"; mode = [ "n" "o" "v" "x" ]; action = "e";   desc = "Next Last Letter"; }
+    { key = "l";       mode = [ "n" "o" "v" "x" ]; action = "b";   desc = "Previous Word"; }
+    { key = "u";       mode = [ "n" "o" "v" "x" ]; action = "w";   desc = "Next First Letter"; }
+    { key = "y";       mode = [ "n" "o" "v" "x" ]; action = "e";   desc = "Next Last Letter"; }
 
-    { key = "h"; mode = [ "n" "o" "v" "x" ]; action = "0";   desc = "Start of Line"; }
-    { key = ","; mode = [ "n" "o" "v" "x" ]; action = "$";   desc = "End of Line"; }
+    { key = "h";       mode = [ "n" "o" "v" "x" ]; action = "0";   desc = "Start of Line"; }
+    { key = ",";       mode = [ "n" "o" "v" "x" ]; action = "$";   desc = "End of Line"; }
 
-    { key = "."; mode = [ "n" "o" "v" "x" ]; action = "%";   desc = "Matching Brackets"; }
+    { key = ".";       mode = [ "n" "o" "v" "x" ]; action = "%";   desc = "Matching Brackets"; }
 
 
 
     # Insert Modes
 
-    { key = "a"; mode = [ "n" "o" "v" "x" ]; action = "o";   desc = "Newline Below then Insert"; }
-    { key = "r"; mode = [ "n" "o" "v" "x" ]; action = "r";   desc = "Replace One Character"; }
-    { key = "s"; mode = [ "n" "o" "v" "x" ]; action = "i";   desc = "Insert Before Cursor"; }
-    { key = "t"; mode = [ "n" "o" "v" "x" ]; action = "a";   desc = "Insert After Cursor"; }
+    { key = "a";       mode = [ "n" "o" "v" "x" ]; action = "o";   desc = "Newline Below then Insert"; }
+    { key = "r";       mode = [ "n" "o" "v" "x" ]; action = "r";   desc = "Replace One Character"; }
+    { key = "s";       mode = [ "n" "o" "v" "x" ]; action = "i";   desc = "Insert Before Cursor"; }
+    { key = "t";       mode = [ "n" "o" "v" "x" ]; action = "a";   desc = "Insert After Cursor"; }
 
-    { key = "A"; mode = [ "n" "o" "v" "x" ]; action = "O";   desc = "Newline Below then Insert"; }
-    { key = "R"; mode = [ "n" "o" "v" "x" ]; action = "R";   desc = "Replace Mode/Multiple"; }
-    { key = "S"; mode = [ "n" "o" "v" "x" ]; action = "I";   desc = "Insert At Start of Line"; }
-    { key = "T"; mode = [ "n" "o" "v" "x" ]; action = "A";   desc = "Insert At End of Line"; }
+    { key = "A";       mode = [ "n" "o" "v" "x" ]; action = "O";   desc = "Newline Below then Insert"; }
+    { key = "R";       mode = [ "n" "o" "v" "x" ]; action = "R";   desc = "Replace Mode/Multiple"; }
+    { key = "S";       mode = [ "n" "o" "v" "x" ]; action = "I";   desc = "Insert At Start of Line"; }
+    { key = "T";       mode = [ "n" "o" "v" "x" ]; action = "A";   desc = "Insert At End of Line"; }
 
 
 
     # Copy, Paste
 
-    { key = "f"; mode = [ "n" "o" "v" "x" ]; action = "y";   desc = "Copy/Yank"; }
-    { key = "p"; mode = [ "n" "o" "v" "x" ]; action = "p";   desc = "Paste"; }
+    { key = "f";       mode = [ "n" "o" "v" "x" ]; action = "y";   desc = "Copy/Yank"; }
+    { key = "p";       mode = [ "n" "o" "v" "x" ]; action = "p";   desc = "Paste"; }
 
 
 
     # Undo/Redo
 
-    { key = "w"; mode = [ "n" "o" "v" "x" ]; action = "u";   desc = "Undo"; }
-    { key = "W"; mode = [ "n" "o" "v" "x" ]; action = "<C-U>";   desc = "Redo"; }
+    { key = "w";       mode = [ "n" "o" "v" "x" ]; action = "u";   desc = "Undo"; }
+    { key = "W";       mode = [ "n" "o" "v" "x" ]; action = "<C-R>";   desc = "Redo"; }
 
 
 
     # Search
 
-    { key = "<C-e>"; mode = [ "n" "o" "v" "x" ]; action = "n";   desc = "Seach Next"; }
-    { key = "<C-i>"; mode = [ "n" "o" "v" "x" ]; action = "N";   desc = "Seach Previous"; }
+    { key = "<C-e>";   mode = [ "n" "o" "v" "x" ]; action = "n";   desc = "Seach Next"; }
+    { key = "<C-i>";   mode = [ "n" "o" "v" "x" ]; action = "N";   desc = "Seach Previous"; }
   ];
 }
